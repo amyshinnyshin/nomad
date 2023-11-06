1100b29b1d76329c1f6deec8fabeb8e317d4d622
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2023-11-05 20:57:45 -0600

    Day 1 - Set Up EJS files

---
+++ controllers/travelPlansController.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const TravelPlans = require("../models/travelPlansModel");
+
+
+//GET all travel plans function
+// const getAllTravelPlans = async (req, res) => {
+//     try {
+//         const allTravelPlans = await TravelPlans.find(); // Use the TravelPlan model to find all plans
+//         res.status(200).json(allTravelPlans);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+
+//GET new travel plans form
+const getNewTravelPlanForm = async (req, res) => {
+    try {
+        res.render("travelPlanForm.ejs")
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+
+//POST new travel plan by creating plan 
+const createTravelPlan = async (req, res) => {
+    const travelPlan = new TravelPlans({
+        planName: req.body.planName, 
+        location: req.body.location,
+        planDescription: req.body.planDescription, 
+        events: [],
+    });
+
+    try {
+        const newTravelPlan = await travelPlan.save();
+        res.status(201).json(newTravelPlan);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+
+//GET one by ID
+const getOneTravelPlan = (req, res) => {
+    res.send(res.plan)
+}
+
+
+
+//PATCH by ID, Updating Plan
+const updateTravelPlan =  async (req, res) => {
+    if (req.body.planName != null) {
+        res.plan.planName = req.body.planName;
+    }
+    if (req.body.location != null) {
+        res.plan.location = req.body.location;
+    }
+    if (req.body.planDescription != null) {
+        res.plan.planDescription = req.body.planDescription;
+    }
+    if (req.body.events != null) {
+        res.plan.events = req.body.events;
+    }
+    try {
+        const updatedTravelPlan = await res.plan.save()
+        res.json(updatedTravelPlan);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+
+
+//DELETE by ID, deleting a plan...
+const deleteTravelPlan = async (req, res) => {
+    try {
+        await res.plan.deleteOne();
+      res.json({ message: 'Deleted travel plan' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+
+
+//–––––––––––––––––––––––––GET By ID
+const getTravelPlansById = async function getTravelPlan(req, res, next) {
+    let plan;
+    try {
+      plan = await TravelPlans.findById(req.params.id)
+      if (plan == null) {
+        return res.status(404).json({ message: 'Cannot find travel plan' })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.plan = plan;
+    next();
+}
+
+
+
+module.exports = { getNewTravelPlanForm, createTravelPlan, getOneTravelPlan, updateTravelPlan, deleteTravelPlan, getTravelPlansById }
